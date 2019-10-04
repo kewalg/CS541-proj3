0b136dd1cd7e37ac0b8278d4700fa4909f379bba
kewalg <kewalgulve007@gmail.com>
2019-10-04 14:44:31 -0400

    basic animation

--- core/src/com/mygdx/mygame/MyGame.java
+++ core/src/com/mygdx/mygame/MyGame.java
@@ -9,36 +9,51 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
-	Texture snake;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		background = new Texture("bg.jpg");
-		snake = new Texture("arrow1.png");
-	}
+    SpriteBatch batch;
+    Texture bg;
+    Texture man[];
+    int manPos = 0;
+    int animate_slow = 0;
 
-	@Override
-	public void render () {
-		batch.begin();
-		batch.draw(background, 0, 0 , Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(snake, Gdx.graphics.getWidth()/2 - snake.getWidth()/2, Gdx.graphics.getHeight()/2 - snake.getHeight()/2);
-		/*Gdx.gl.glClearColor(0,0,0,1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		ShapeRenderer shapeRenderer = new ShapeRenderer();
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(Color.WHITE);
-		shapeRenderer.rect(10,10,90, 90);
-		shapeRenderer.end();
-		*/
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		snake.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        bg = new Texture("bg.png");
+        man = new Texture[10];
+        man[0] = new Texture("frame_1.png");
+        man[1] = new Texture("frame_2.png");
+        man[2] = new Texture("frame_3.png");
+        man[3] = new Texture("frame_4.png");
+        man[4] = new Texture("frame_5.png");
+        man[5] = new Texture("frame_6.png");
+        man[6] = new Texture("frame_7.png");
+        man[7] = new Texture("frame_8.png");
+        man[8] = new Texture("frame_9.png");
+        man[9] = new Texture("frame_10.png");
+    }
+
+    @Override
+    public void render() {
+        batch.begin();
+        batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if (animate_slow < 4) {
+            animate_slow++;
+        } else {
+            animate_slow = 0;
+            if (manPos < 9) {
+                manPos++;
+            } else {
+                manPos = 0;
+            }
+        }
+
+        batch.draw(man[manPos], Gdx.graphics.getWidth() / 2 - man[manPos].getWidth() / 2, Gdx.graphics.getHeight() / 2 - man[manPos].getHeight() / 2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
