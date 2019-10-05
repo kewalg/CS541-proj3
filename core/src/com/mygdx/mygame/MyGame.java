2373563b764a6ae9c7dbc52e8dd9dcb3bb70d276
kewalg <kewalgulve007@gmail.com>
2019-10-04 22:53:30 -0400

    character animation updated

--- core/src/com/mygdx/mygame/MyGame.java
+++ core/src/com/mygdx/mygame/MyGame.java
@@ -11,25 +11,32 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class MyGame extends ApplicationAdapter {
     SpriteBatch batch;
     Texture bg;
-    Texture man[];
-    int manPos = 0;
-    int animate_slow = 0;
+    Texture hero[];
+    int heroPos = 0;
+    int hero_AnimateSlow = 0;
+    float gravity = 0.5f;
+    float velocity = 0;
+    float hero_YState;
+    float hero_XState;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         bg = new Texture("bg.png");
-        man = new Texture[10];
-        man[0] = new Texture("frame_1.png");
-        man[1] = new Texture("frame_2.png");
-        man[2] = new Texture("frame_3.png");
-        man[3] = new Texture("frame_4.png");
-        man[4] = new Texture("frame_5.png");
-        man[5] = new Texture("frame_6.png");
-        man[6] = new Texture("frame_7.png");
-        man[7] = new Texture("frame_8.png");
-        man[8] = new Texture("frame_9.png");
-        man[9] = new Texture("frame_10.png");
+        hero = new Texture[10];
+        hero_YState = Gdx.graphics.getHeight() / 2;
+        hero_XState = 0;
+        hero[0] = new Texture("frame_1.png");
+        hero[1] = new Texture("frame_2.png");
+        hero[2] = new Texture("frame_3.png");
+        hero[3] = new Texture("frame_4.png");
+        hero[4] = new Texture("frame_5.png");
+        hero[5] = new Texture("frame_6.png");
+        hero[6] = new Texture("frame_7.png");
+        hero[7] = new Texture("frame_8.png");
+        hero[8] = new Texture("frame_9.png");
+        hero[9] = new Texture("frame_10.png");
     }
 
     @Override
@@ -37,18 +44,34 @@ public class MyGame extends ApplicationAdapter {
         batch.begin();
         batch.draw(bg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        if (animate_slow < 4) {
-            animate_slow++;
+/*
+        if (Gdx.input.isTouched()){
+
+        }
+*/
+
+        if (hero_AnimateSlow < 4) {
+            hero_AnimateSlow++;
         } else {
-            animate_slow = 0;
-            if (manPos < 9) {
-                manPos++;
+            hero_AnimateSlow = 0;
+            if (heroPos < 9) {
+                heroPos++;
             } else {
-                manPos = 0;
+                heroPos = 0;
             }
         }
 
-        batch.draw(man[manPos], Gdx.graphics.getWidth() / 2 - man[manPos].getWidth() / 2, Gdx.graphics.getHeight() / 2 - man[manPos].getHeight() / 2);
+
+        velocity = velocity + gravity;
+        hero_YState = hero_YState - velocity;
+        if (hero_YState <= 0) {
+            hero_YState = 0;
+        }
+
+
+        //batch.draw(hero[heroPos], Gdx.graphics.getWidth() / 2 - hero[heroPos].getWidth() / 2, Gdx.graphics.getHeight() / 2 - hero[heroPos].getHeight() / 2);
+        //batch.draw(hero[heroPos], Gdx.graphics.getWidth() / (2) - hero[heroPos].getWidth() / 2, hero_YState);
+        batch.draw(hero[heroPos], hero_XState ,hero_YState);
         batch.end();
     }
 
